18a57808bfc3f2f3770ac769728e42f2aed7aff8
Akash Agrawal <akashji93685@gmail.com>
2026-06-16 14:43:28 +0530

    LC125 Valid Palindrome

---
+++ src/LC125_ValidPalindrome.java
@@ -0,0 +1,19 @@
+public class LC125_ValidPalindrome {
+    public static boolean check(String s){
+        String ans  = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int start = 0;
+        int end = ans.length()-1;
+        while(start<=end){
+            if(ans.charAt(start)!= ans.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+
+   //[^a-zA-Z0-9] regex hai ye iska mtlv a-z or A-Z or 0-9 ko chorke sb ko hta do or lowercase me bdlo
+
+}
