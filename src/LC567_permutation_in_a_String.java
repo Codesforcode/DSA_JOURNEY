427efd036255153f19d0164ce66101462440dc9d
Akash Agrawal <akashji93685@gmail.com>
2026-06-18 19:42:38 +0530

    LCLC567_permutation_in_a_String

---
+++ src/LC567_permutation_in_a_String.java
@@ -0,0 +1,34 @@
+public class LC567_permutation_in_a_String {
+    public  boolean check(int [] arr1 , int []arr2){
+        for(int i = 0; i<26 ;i++){
+            if(arr1[i]!= arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public  boolean checkPermutation(String s1 , String s2){
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int [] arr1  = new int [26];
+        int [] arr2 = new int [26];
+        int k = s1.length();
+        for(int i = 0 ;i<k;i++){
+            arr1[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
+        }
+        if(check(arr1,arr2)){
+            return true;
+        }
+        for(int i = k ;i<s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+            arr2[s2.charAt(i-k)-'a']--;
+            if(check(arr1,arr2)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
